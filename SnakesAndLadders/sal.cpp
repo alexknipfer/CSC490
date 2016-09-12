9581aa4b5ebe54c6bb1c02751f96cf53b0c4946c
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-09-12 13:11:19 -0500

    completed program

--- SnakesAndLadders/sal.cpp
+++ SnakesAndLadders/sal.cpp
@@ -19,12 +19,14 @@ int main(){
 
 //******************************************************************************
 
+  //initialize current position to starting position at 0
 SnakesAndLadders::SnakesAndLadders(){
   currentPosition = 0;
 }
 
 //******************************************************************************
 
+  //take input file name from console
 void SnakesAndLadders::getFileName(ifstream &myFile){
   string fileName;
 
@@ -46,6 +48,7 @@ void SnakesAndLadders::getFileName(ifstream &myFile){
 
 //******************************************************************************
 
+  //get the board size and snakes and ladders positions
 void SnakesAndLadders::getBoardLayout(ifstream &myFile){
   int start;
   char typeSnakeOrLadder;
@@ -67,22 +70,32 @@ void SnakesAndLadders::getBoardLayout(ifstream &myFile){
 }
 //******************************************************************************
 
+  //this function gets the amount of sequences and all the dice rolls
+  //for each sequence
 void SnakesAndLadders::getDiceRolls(ifstream &myFile){
   int numOfSequences;
   int numOfRolls;
   int diceValue;
 
+    //get amount of dice sequences from file
   myFile >> numOfSequences;
 
+    //loop through number of sequences to get rolls
   for(int x = 0; x < numOfSequences; x++){
     myFile >> numOfRolls;
-    //cout << numOfRolls << endl;
+
+      //now loop through rolls on current sequence
+      //add the value of roll to the dice vector
     for(int y = 0; y < numOfRolls; y++){
       myFile >> diceValue;
       diceValues.push_back(diceValue);
     }
 
+      //go analyze the dice rolls in current sequence
     analyzeDiceRolls();
+
+      //if the position is >= board size then the sequence of rolls is a Winner
+      //add win or loose to vector to print later after all sequences are complete
     if(currentPosition >= maxSquareNumber){
       gameResults.push_back("Winner!");
     }
@@ -90,6 +103,7 @@ void SnakesAndLadders::getDiceRolls(ifstream &myFile){
       gameResults.push_back("Did not make it!");
     }
 
+      //reset position and dice vector for next sequence
     diceValues.clear();
     currentPosition = 0;
   }
@@ -97,19 +111,22 @@ void SnakesAndLadders::getDiceRolls(ifstream &myFile){
 
 //******************************************************************************
 
+  //adjust position for the current sequence of dice rolls
 void SnakesAndLadders::analyzeDiceRolls(){
   for(int x = 0; x < diceValues.size(); x++){
-    cout << currentPosition << endl;
     currentPosition += diceValues[x];
     if(currentPosition >= maxSquareNumber){
       return;
     }
+      //go see if the roll landed on a snake or ladder
     analyzePosition();
   }
 }
 
 //******************************************************************************
 
+  //determine if the roll landed on a snake or ladder and reset the position
+  //to new position if landed on either
 void SnakesAndLadders::analyzePosition(){
   for(int x = 0; x < startSquare.size(); x++){
     if(currentPosition == startSquare[x]){
@@ -120,6 +137,7 @@ void SnakesAndLadders::analyzePosition(){
 
 //******************************************************************************
 
+  //print the game results for all sequences
 void SnakesAndLadders::printResults(){
   for(int x = 0; x < gameResults.size(); x++){
     cout << gameResults[x] << endl;
