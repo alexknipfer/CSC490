6b6aaa4877bdc6dc72fe546fd0d998c494c10f78
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-04 15:11:22 -0500

    analyzes even size boards to figure out how manys squares will fit inside

--- SquareFill/square.cpp
+++ SquareFill/square.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int findMinimum(int squareSize){
+int findEvenMinimum(int squareSize){
   int minimum;
   int evenMinimum;
 
@@ -14,21 +14,50 @@ int findMinimum(int squareSize){
     evenMinimum = 4;
     return evenMinimum;
   }
+
   return 0;
+}
+
+int findOddMinimum(int squareSize, unsigned int n){
+
+  if(n <= 3){
+    return n;
+  }
+
+  int res = n;
+
+  for(int x = 1; x <= squareSize - 1; x++){
+    int temp = x*x;
+    if(temp > n){
+      break;
+    }
+    else{
+      res = min(res, 1+ findOddMinimum(squareSize, n - temp));
+    }
+  }
+  return res;
 }
 
 void getCoordinates(int squareSize, int minimumSquares){
   vector<int> smallSquareSize;
-  vector<int> x;
-  vector<int> y;
+  vector<int> xCoor;
+  vector<int> yCoor;
 
   if(minimumSquares == 4){
     for(int x = 0; x < 4; x++){
       smallSquareSize.push_back(squareSize / 2);
     }
+    xCoor.push_back(0);
+    yCoor.push_back(0);
+    xCoor.push_back(squareSize / 2);
+    yCoor.push_back(0);
+    xCoor.push_back(0);
+    yCoor.push_back(squareSize / 2);
+    xCoor.push_back(squareSize / 2);
+    xCoor.push_back(squareSize / 2);
   }
   for(int x = 0; x < smallSquareSize.size(); x++){
-    cout << smallSquareSize[x] << endl;
+    cout << smallSquareSize[x] << " " << xCoor[x] << " " << yCoor[x] << endl;
   }
 }
 
@@ -36,9 +65,21 @@ int main(){
   int squareSize;
   int minimumSquares;
 
+  char board[21][21];
+
   cin >> squareSize;
 
-  minimumSquares = findMinimum(squareSize);
-  getCoordinates(squareSize, minimumSquares);
+  if(squareSize % 2 == 0){
+    minimumSquares = findEvenMinimum(squareSize);
+    cout << minimumSquares << endl;
+    getCoordinates(squareSize, minimumSquares);
+
+  }
+
+  else{
+    minimumSquares = findOddMinimum(squareSize, squareSize*squareSize);
+    cout << minimumSquares << endl;
+  }
+
   return 0;
 }
