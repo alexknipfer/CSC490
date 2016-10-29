653ba94495ed9b295e77bede07333b74edf69ede
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-29 18:10:21 -0500

    read in data

--- lizards/lizards.cpp
+++ lizards/lizards.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int PilarMap[25][25];
+char PilarMap[25][25];
 char LizardMap[25][25];
 
 int main(){
-  
+  int testCases;
+  int n;
+  int d;
+  string line;
+
+  cin >> testCases;
+
+  for(int x = 0; x < testCases; x++){
+    cin >> n >> d;
+
+    cin >> line;
+
+    cout << line << endl;
+    break;
+    /*for(int i = 0; i < n; i++){
+      cin >> PilarMap[i];
+    }
+
+    for(int j = 0; j < n; j++){
+      cin >> LizardMap[j];
+    }
+  }
+
+  for(int x = 0; x < n; x++){
+    cout << PilarMap[x] << " ";
+  }*/
+}
 
   return 0;
 }
