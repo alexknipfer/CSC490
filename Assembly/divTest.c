f141c48c6ac98576ea4cbd731b625a52e726c49a
Alex Knipfer <alexanderknipfer@gmail.com>
2016-11-25 23:00:18 -0600

    update

--- Assembly/divTest.c
+++ Assembly/divTest.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
 void foo(int n){
-<<<<<<< HEAD
   if(n<2){
     
   }
 }
 
 int main(void){
-  foo(2);
-=======
-  //do something
-}
-
-int main(void){
   foo(4);
->>>>>>> b08852f5d5e306d2512e6ef226d9ebda1c32cc30
   return 0;
 }
--- Assembly/divTest.s
+++ Assembly/divTest.s
@@ -1,51 +1,41 @@
-<<<<<<< HEAD
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 12
-	.globl	_foo
-	.align	4, 0x90
-_foo:                                   ## @foo
-	pushq	%rbp
-	movq	%rsp, %rbp
-	movl	%edi, -4(%rbp)
-	cmpl	$2, -4(%rbp)
-	retq
-
-_main:                                  ## @main
-	subq	$16, %rsp
-	movl	$2, %edi
-	movl	$0, -4(%rbp)
-	callq	_foo
-	xorl	%eax, %eax
-	addq	$16, %rsp
-	popq	%rbp
-	retq
-
-.subsections_via_symbols
-=======
 	.file	"divTest.c"
 	.text
 	.globl	foo
 	.type	foo, @function
 foo:
+.LFB0:
+	.cfi_startproc
 	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
 	movl	%edi, -4(%rbp)
 	nop
 	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
+	.cfi_endproc
+.LFE0:
 	.size	foo, .-foo
 	.globl	main
 	.type	main, @function
 main:
+.LFB1:
+	.cfi_startproc
 	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
 	movl	$4, %edi
 	call	foo
 	movl	$0, %eax
 	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
+	.cfi_endproc
 .LFE1:
 	.size	main, .-main
 	.ident	"GCC: (Solus) 6.2.0"
 	.section	.note.GNU-stack,"",@progbits
->>>>>>> b08852f5d5e306d2512e6ef226d9ebda1c32cc30
